fd1aeae88c509c5e2427d19f78926975393d6a41
codewurld <sholaq@gmail.com>
2021-12-13 16:51:46 +0000

    added form in views and connected to server.js

--- server.js
+++ server.js
@@ -3,5 +3,13 @@ const express = require('express');
 // instance of express
 const app = express()
 
+app.set('view engine', 'ejs')
+
+// route
+// render index file to display properties 
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 // set up server on port
 app.listen(process.env.PORT || 8080);
